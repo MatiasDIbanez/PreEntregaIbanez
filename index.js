c987311d194eddcadba2903d0a767fdbd9e233e4
matias_ibanez <matias_ibanez@hotmail.com>
2023-07-24 15:28:10 -0300

    Nuevo

--- index.js
+++ index.js
@@ -21,62 +21,7 @@ function Persona(nombre, apellido, fechaNacimiento) {
 
  //let usuarios = [{ persona }];
 
-//Productos
-function ItemConstructora(nombre, categoria, stock,precio) {
-  this.nombre = nombre;
-  this.categoria = categoria;
-  this.stock = stock;
-}
-const item1 = new ItemConstructora("Carne suave", "Clasica", 100);
-const item2 = new ItemConstructora("Carne picante", "Clasica", 100);
-const item3 = new ItemConstructora("Pollo", "Clasica", 100);
-const item4 = new ItemConstructora("Pollo con salsa blanca", "Clasica", 100);
-const item5 = new ItemConstructora("Roquefort con jamón", "Clasica", 100);
-const item6 = new ItemConstructora("Jamón y queso", "Clasica", 100);
-const item7 = new ItemConstructora("Caprese", "Vegetariana", 100);
-const item8 = new ItemConstructora("Queso y cebolla", "Vegetariana", 100);
-const item9 = new ItemConstructora("Humita", "Vegetariana", 100);
-const item10 = new ItemConstructora("Calabaza", "Vegetariana", 100);
-const item11 = new ItemConstructora("Verdura", "Vegetariana", 100);
-const item12 = new ItemConstructora("Diabla", "Especial", 100);
-const item13 = new ItemConstructora("Cuatro queso", "Especial", 100);
-const item14 = new ItemConstructora("Cheese burger", "Especial", 100);
-const item15 = new ItemConstructora("No pollo", "Vegana", 80);
-const item16 = new ItemConstructora("Caprese", "Vegana", 80);
-const item17 = new ItemConstructora("Agridulce", "Vegana", 80);
-const item18 = new ItemConstructora("New mix", "Vegana", 80);
-const item19 = new ItemConstructora("Membrillo", "Dulce", 30);
-const item20 = new ItemConstructora("Batata", "Dulce", 30);
-const item21 = new ItemConstructora("Chocotorta", "Postre", 10);
-const item22 = new ItemConstructora("Bomba oreo", "Postre", 10);
-const item23 = new ItemConstructora("Brownie explotado", "Postre", 10);
-const item24 = new ItemConstructora("Vegano", "Postre", 10);
-let productos = [
-  item1,
-  item2,
-  item3,
-  item4,
-  item5,
-  item6,
-  item7,
-  item8,
-  item9,
-  item10,
-  item11,
-  item12,
-  item13,
-  item14,
-  item15,
-  item16,
-  item17,
-  item18,
-  item19,
-  item20,
-  item21,
-  item22,
-  item23,
-  item24,
-];
+
 
 //Funcion filtrado
 function FilterCategoria(categoria){
